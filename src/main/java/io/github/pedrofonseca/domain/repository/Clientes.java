6d28b54f04c7dda7e91628c23fbd700865d53841
pefonseca <pedrofonunes@gmail.com>
2022-11-05 15:37:48 -0300

    CRUD with JDBCTemplate finished

--- src/main/java/io/github/pedrofonseca/domain/repository/Clientes.java
+++ src/main/java/io/github/pedrofonseca/domain/repository/Clientes.java
@@ -15,6 +15,8 @@ public class Clientes {
 
     private static final String INSERT = "INSERT INTO CLIENTE (nome) VALUES (?) ";
     private static final String SELECT_ALL = "SELECT * FROM CLIENTE ";
+    private static final String UPDATE  = "UPDATE CLIENTE SET nome = ? WHERE id = ? ";
+    private static final String DELETE = "DELETE FROM CLIENTE WHERE id = ? ";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -24,14 +26,38 @@ public class Clientes {
         return cliente;
     }
 
+    public Cliente update(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, cliente.getNome(), cliente.getId());
+        return cliente;
+    }
+
+    public void deletar(Cliente cliente) {
+        deletar(cliente.getId());
+    }
+
+    public void deletar(Integer id) {
+        jdbcTemplate.update(DELETE, id);
+    }
+
+    public List<Cliente> findByName(String nome) {
+        return jdbcTemplate.query(
+                SELECT_ALL.concat("WHERE nome LIKE ? "),
+                new Object[]{"%" + nome + "%"},
+                getRowMapper());
+    }
+
     public List<Cliente> findAll() {
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+        return jdbcTemplate.query(SELECT_ALL, getRowMapper());
+    }
+
+    private static RowMapper<Cliente> getRowMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 Integer id = resultSet.getInt("id");
                 String nome = resultSet.getString("nome");
                 return Cliente.builder().id(id).nome(nome).build();
             }
-        });
+        };
     }
 }
