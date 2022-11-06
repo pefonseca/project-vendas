46e710560d0ea54afcaa9b3b14f659eaeac73786
pefonseca <pedrofonunes@gmail.com>
2022-11-06 15:11:40 -0300

    JpaRepository with SpringData in CRUD

--- src/main/java/io/github/pedrofonseca/VendasApplication.java
+++ src/main/java/io/github/pedrofonseca/VendasApplication.java
@@ -20,34 +20,8 @@ public class VendasApplication {
             clientes.save(new Cliente(null,"Pedro"));
             clientes.save(new Cliente(null,"Bento"));
 
-            List<Cliente> clienteList = clientes.findAll();
-            clienteList.forEach(System.out::println);
-
-            System.out.println("\nAtualizando Clientes.");
-            clienteList.forEach(c -> {
-                c.setNome(c.getNome() + " atualizado.");
-                clientes.update(c);
-            });
-
-            System.out.println("\nBuscando Clientes Atualizandos.");
-            clienteList = clientes.findAll();
-            clienteList.forEach(System.out::println);
-
-            System.out.println("\nBuscando Clientes Por Nome.");
-            clientes.findByName("Ped").forEach(System.out::println);
-
-            System.out.println("\nDeletando Clientes.");
-            clientes.findAll().forEach(c -> {
-                clientes.deletar(c);
-            });
-
-            clienteList = clientes.findAll();
-
-            if(clienteList.isEmpty()) {
-                System.out.println("Nenhum cliente encontrado.");
-            } else {
-                clienteList.forEach(System.out::println);
-            }
+            boolean existe = clientes.existsByNome("eae");
+            System.out.println("Existe um cliente com o nome Pedro? " + existe);
         };
     }
 
--- src/main/java/io/github/pedrofonseca/domain/repository/Clientes.java
+++ src/main/java/io/github/pedrofonseca/domain/repository/Clientes.java
@@ -1,58 +1,15 @@
 package io.github.pedrofonseca.domain.repository;
 
 import io.github.pedrofonseca.domain.entity.Cliente;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
-public class Clientes {
+public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-    @Autowired
-    private EntityManager entityManager;
-
-    @Transactional
-    public Cliente save(Cliente cliente) {
-        entityManager.persist(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public Cliente update(Cliente cliente) {
-        entityManager.merge(cliente);
-        return cliente;
-    }
-
-    @Transactional
-    public void deletar(Cliente cliente) {
-        if(!entityManager.contains(cliente)) {
-            cliente = entityManager.merge(cliente);
-        }
-        entityManager.remove(cliente);
-    }
-
-    @Transactional
-    public void deletar(Integer id) {
-        Cliente cliente = entityManager.find(Cliente.class, id);
-        deletar(cliente);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> findByName(String nome) {
-        String jpql = "SELECT c FROM Cliente c WHERE c.nome LIKE :nome";
-        TypedQuery<Cliente> query = entityManager.createQuery(jpql, Cliente.class);
-        query.setParameter("nome", "%" + nome + "%");
-        return query.getResultList();
-    }
-
-    @Transactional(readOnly = true)
-    public List<Cliente> findAll() {
-        return entityManager
-                .createQuery("FROM Cliente", Cliente.class)
-                .getResultList();
-    }
+    List<Cliente> findByNomeLike(String nome);
+    List<Cliente> findByNomeOrIdOrderById(String nome, Integer id);
+    boolean existsByNome(String nome);
 }
